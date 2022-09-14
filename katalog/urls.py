10016a8d0618f136c838f01a134b10fe2482fe3b
Dhafiano Fadeyka Ghani Wiweko <dhafiano.fadeyka@ui.ac.id>
2022-09-14 19:51:48 +0700

    update tugas 2

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import showkatalog
+
+app_name = "katalog"
+
+urlpatterns = [
+    path('', showkatalog, name='katalog'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
+def showkatalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_katalog,
+    'name': 'Dhafiano Fadeyka Ghani Wiweko',
+    'studentid': ''
+}
+    return render(request, "katalog.html", context)
 # TODO: Create your views here.
